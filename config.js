09c0add450a3f0ee8912fd9a96c2ef80105157ce
Mourad Hamza <37896042+mourad-hamza@users.noreply.github.com>
2025-02-21 18:06:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   dbType: 'mysql', // Change to 'sqlite' for SQLite
   mysqlConfig: {
-    host: "DBIPADDRESS",
+    host: 'DBIPADDRESS',
     user: 'root',
     password: 'P@ssw0rdP@ssw0rd',
     database: 'survey_db' // Change to your desired database name
